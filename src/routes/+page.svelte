9274701c30743aa2b4ea30d370209e8d18ac54d4
BeratK2 <bales1801@gmail.com>
2023-03-07 23:48:01 -0500

    Landing page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,86 @@
+<main class="index">
+  <h1>DogQuiry</h1>
+  <!-- svelte-ignore a11y-missing-attribute -->
+  <img
+    src="https://www.alltech.com/sites/default/files/2021-08/Pet%20Mobile%20Header_0.png"
+  />
+  <div class="form-container">
+    <input placeholder="Search for a dog breed" />
+    <button>Search This Breed</button>
+  </div>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta quasi quo
+    dolorum aut, laborum eligendi aliquam tempora dicta optio, adipisci maiores
+    voluptatibus. Dignissimos animi explicabo culpa, eligendi illo recusandae
+    alias.
+  </p>
+</main>
+
+<style>
+  .index {
+    display: block;
+    margin: 20px auto;
+    text-align: center;
+  }
+  h1 {
+    font-size: 70px;
+  }
+  img {
+    width: 100%;
+    height: 10%;
+    object-fit: contain;
+  }
+  p {
+    font-weight: 500;
+  }
+  .form-container {
+    display: flex;
+    flex-direction: column;
+    align-self: center;
+    align-items: center;
+    margin-top: 50px;
+  }
+  input {
+    width: 650px;
+    height: 59px;
+    background: rgb(241, 241, 241);
+    border: 1px solid rgba(117, 117, 117, 0.5);
+    border-radius: 50px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    font-size: 20px;
+    text-align: left;
+    padding-top: 5px;
+    padding-left: 20px;
+  }
+  input:focus {
+    outline: none;
+    box-shadow: 0px 4px 4px rgba(34, 130, 255, 0.25);
+    border: 1px solid rgba(34, 130, 255, 0.25);
+  }
+  input::target-text {
+    margin-left: 10px;
+  }
+  button {
+    width: 361.65px;
+    height: 64.18px;
+    margin-top: 50px;
+    padding-top: 5px;
+    background: #e66464;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    border: 0.5px solid rgba(231, 80, 80, 0.5);
+    border-radius: 30px;
+    color: whitesmoke;
+    font-size: 30px;
+    font-weight: bold;
+  }
+  button:hover{
+    box-shadow: 0px 4px 4px rgba(231, 80, 80, 0.25);
+  }
+  button:focus{
+    box-shadow: 0px 4px 4px rgba(231, 80, 80, 0.5);
+    outline: none;
+  }
+  p{
+    margin-top: 50px;
+  }
+</style>
---
+++ src/routes/dog/+page.svelte
@@ -0,0 +1,3 @@
+<main>
+    DOGGGS
+</main>
